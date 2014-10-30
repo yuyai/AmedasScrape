74c0a25a4496f5707cd65c297aed0e27350e9408
岩田裕也 <yuya@iwatayuuya-no-MacBook-Pro.local>
2014-10-31 01:18:33 +0900

    Add UIWebView

--- AmedasScrape/ViewController.m
+++ AmedasScrape/ViewController.m
@@ -17,6 +17,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // UIWebViewのインスタンス化
+    CGRect rect = self.view.frame;
+    UIWebView *webView = [[UIWebView alloc]initWithFrame:rect];
+    
+    // Webページの大きさを自動的に画面にフィットさせる
+    webView.scalesPageToFit = YES;
+    webView.scrollView.scrollEnabled = YES;
+    
+    // デリゲートを指定
+    webView.delegate = self;
+    
+    // URLを指定
+    NSURL *url = [NSURL URLWithString:@"http://www.jma.go.jp/jp/amedas_h/today-40341.html?areaCode=000&groupCode=26"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    // リクエストを投げる
+    [webView loadRequest:request];
+    
+    // UIWebViewのインスタンスをビューに追加
+    [self.view addSubview:webView];
+}
+
+/**
+ * Webページのロード時にインジケータを動かす
+ */
+- (void)webViewDidStartLoad:(UIWebView*)webView
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+
+/**
+ * Webページのロード完了時にインジケータを非表示にする
+ */
+- (void)webViewDidFinishLoad:(UIWebView*)webView
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
 }
 
 - (void)didReceiveMemoryWarning {
